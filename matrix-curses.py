849cf1d193d7c96454e420920c97df99aeb7f6ed
eduardo hoefel <eduardo.hoefel@acad.pucrs.br>
2016-04-26 00:19:32 -0300

    now the text changes speed every time it resets
    code is nicier

--- matrix-curses.py
+++ matrix-curses.py
@@ -38,26 +38,31 @@ encoding = locale.getpreferredencoding()
 ########################################################################
 # TUNABLES
 
-DROPPING_CHARS = 50
 MIN_SPEED = 1
-MAX_SPEED = 7
-RANDOM_CLEANUP = 100
+MAX_SPEED = 3
 FPS = 25
 SLEEP_MILLIS = 1.0/FPS
-USE_COLORS = False
 SCREENSAVER_MODE = True
 MATRIX_CODE_CHARS = "01"
 MATRIX_CODE_CHARS = list(MATRIX_CODE_CHARS)
 
 ########################################################################
-# CODE
+# COLORS
 
 COLOR_CHAR_NORMAL = 1
 COLOR_CHAR_HIGHLIGHT = 2
-COLOR_WINDOW = 3
+COLOR_CHAR_CLEAR = 3
 
-NORMAL_ATTR = curses.A_NORMAL
-HIGHLIGHT_ATTR = curses.A_REVERSE
+COLOR_GREEN = 46
+COLOR_BLACK = 16
+COLOR_RED = 196
+COLOR_BLUE = 21
+COLOR_YELLOW = 226
+COLOR_PURPLE = 201
+COLOR_CYAN = 51
+COLOR_WHITE = 231
+COLOR_GRAY_95 = 59
+
 
 CLEAR_STR = ' '
 
@@ -65,50 +70,41 @@ class FallingChar(object):
     
     def __init__(self, WINDOW_HEIGHT, WINDOW_WIDTH, x):
         self.x = x
-        self.speed = 1
         self.char = random.choice(MATRIX_CODE_CHARS)
         self.WINDOW_HEIGHT = WINDOW_HEIGHT
+        self.length = 1
         self.reset()
         self.y = randint(0, WINDOW_WIDTH // 3)
-        self.max_length = randint(5, self.WINDOW_HEIGHT // 2)
-        self.length = 0
     
     def reset(self):
         # self.length = 0
+        self.max_length = randint(5, self.WINDOW_HEIGHT // 2)
         self.y = 0
-        self.speed = randint(1, 3)
+        self.speed = randint(MIN_SPEED, MAX_SPEED)
     
-    def tick(self, scr, steps):
+    def tick(self, steps):
+        scr = FallingChar.scr
         if self.advances(steps):
-            scr.addstr(self.y, self.x, self.char, NORMAL_ATTR)
-            self.y += 1
-            if self.y >= self.WINDOW_HEIGHT:
-               self.reset() 
+            scr.addstr(self.y, self.x, self.char, curses.color_pair(COLOR_CHAR_NORMAL))
+            self.move_y()
             self.char = random.choice(MATRIX_CODE_CHARS)
-            scr.addstr(self.y, self.x, self.char, HIGHLIGHT_ATTR)
-            if self.length < self.max_length:
-                self.length += 1
-            else:
-                clear_y = self.y - self.length
-                if clear_y < 0:
-                    clear_y += self.WINDOW_HEIGHT
-                scr.addstr(clear_y, self.x, CLEAR_STR, NORMAL_ATTR)
-            # if window was resized and char is out of bounds, reset
-            # self.out_of_bounds_reset(width, height)
-            # # make previous char curses.A_NORMAL
-            # if USE_COLORS:
-                # scr.addstr(self.y, self.x, self.char, curses.color_pair(COLOR_CHAR_NORMAL))
-            # else:
-                # scr.addstr(self.y, self.x, self.char, curses.A_NORMAL)
-                
-            # # choose new char and draw it A_REVERSE if not out of bounds
-            # self.char = random.choice(FallingChar.matrixchr).encode(encoding)
-            # self.y += 1
-            # if not self.out_of_bounds_reset(width, height):
-                # if USE_COLORS:
-                    # scr.addstr(self.y, self.x, self.char, curses.color_pair(COLOR_CHAR_HIGHLIGHT))
-                # else:
-                    # scr.addstr(self.y, self.x, self.char, curses.A_REVERSE)
+            scr.addstr(self.y, self.x, self.char, curses.color_pair(COLOR_CHAR_HIGHLIGHT))
+            self.length += 1
+            if self.length > self.max_length:
+                self.clear_last_char()
+
+    def move_y(self):
+        self.y += 1
+        if self.y >= self.WINDOW_HEIGHT:
+            self.reset()
+
+    def clear_last_char(self):
+        scr = FallingChar.scr
+        clear_y = self.y - self.length
+        if clear_y < 0:
+            clear_y += self.WINDOW_HEIGHT
+        scr.addstr(clear_y, self.x, CLEAR_STR, curses.color_pair(COLOR_CHAR_CLEAR))
+        self.length -= 1
     
     def advances(self, steps):
         if steps % (self.speed) == 0:
@@ -116,7 +112,6 @@ class FallingChar(object):
         return False
     
     def step(self, steps, scr):
-       
         return -1, -1, None
 
 # we don't need a good PRNG, just something that looks a bit random.
@@ -140,16 +135,16 @@ def randint(_min, _max):
 def main():
     steps = 0
     scr = curses.initscr()
+    FallingChar.scr = scr
     scr.nodelay(1)
     curses.curs_set(0)
     curses.noecho()
     
-    if USE_COLORS:
-        curses.start_color()
-        curses.use_default_colors()
-        curses.init_pair(COLOR_CHAR_NORMAL, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(COLOR_CHAR_HIGHLIGHT, curses.COLOR_WHITE, curses.COLOR_GREEN)
-        curses.init_pair(COLOR_WINDOW, curses.COLOR_GREEN, curses.COLOR_GREEN)
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(COLOR_CHAR_NORMAL, COLOR_GREEN, COLOR_BLACK)
+    curses.init_pair(COLOR_CHAR_HIGHLIGHT, COLOR_BLACK, COLOR_GREEN)
+    curses.init_pair(COLOR_CHAR_CLEAR, COLOR_GRAY_95, COLOR_BLACK)
     
     height, width = scr.getmaxyx()    
     lines = []
@@ -162,7 +157,7 @@ def main():
     while True:
         height, width = scr.getmaxyx()
         for line in lines:
-            line.tick(scr, steps)
+            line.tick(steps)
         # for i in range(RANDOM_CLEANUP):
             # x = randint(0, width-1)
             # y = randint(0, height-1)
